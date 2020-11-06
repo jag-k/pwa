d302832ef4e3302f4ed3dc0f2f453b88d9154eef
jagk <jag-k58@ya.ru>
2020-11-06 15:46:47 +0300

    First Version

---
+++ sw.js
@@ -0,0 +1,31 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        // '/index.html',
+        // '/shower/',
+        // '/shower/shower.js',
+        // '/shower/themes/ribbon/styles/styles.css',
+        // '/pictures/',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request).then((response) => {
+        let responseClone = response.clone();
+        caches.open('v1').then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      });
+    }).catch(() => {
+      return event.request
+    })
+  );
+});
